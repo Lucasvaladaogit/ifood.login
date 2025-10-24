35f788f7495180b3fd60f5317d05ba2cd544c750
Lucasvaladaogit <28891@raphaeldisanto.com.br>
2025-10-24 08:06:55 -0300

    Add files via upload

--- ifood/src/App.jsx
+++ ifood/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import logo1 from'./assets/logo.png'
+import google from './assets/g.png'
+import face from './assets/face.png'
+import app from './assets/apple.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,12 +23,26 @@ function App() {
 
       <div className="card">
         <h1>Login</h1>
-        <input type="Usuario" id='Usuario' />
-        <input type="Senha" id='Senha' />
-        <input type="checkbox" name="Manter conectado" id="check" />
-        <a href="http://"></a>
+        <input type="Usuario" id='Usuario' placeholder="usuario" />
+        <input type="Senha" id='Senha' placeholder="senha" />
         <button>Entrar</button>
-        <p>logar com</p>
+        <p id="logar">Logar com</p>
+        <a href="https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com%3Fhl%3Dpt-BR&dsh=S-1809534418%3A1761302136200098&ec=GAlA8wE&flowEntry=AddSession&flowName=GlifWebSignIn&hl=pt-BR">
+        <div>
+          
+          <img src={google} alt="" id="logo" />
+        </div>
+        </a>
+        <a href="https://www.facebook.com/?locale=pt_BR">
+        <div>
+          <img src={face} alt="" id="face" />
+        </div>
+        </a>
+         <a href="https://account.apple.com/sign-in">
+        <div>
+          <img src={app} alt="" id="face" />
+        </div>
+        </a>
 
       </div>
     </>
